a0303c94cd523014801454f1dea7e5638b0aaf7a
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-08-11 11:50:07 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  if (arr.find(game => game.result === "W") !== undefined){
+  return arr.find(game => game.result === "W").year}
+  else{
+    return undefined
+  }
+}
